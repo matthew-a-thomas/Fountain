b9f6159cd978daad59bcf434e57a43e4579d6e97
Matt Thomas <me@matthewathomas.com>
2021-07-12 16:00:23 -0400

    Demonstrate solving in place as a way to save disk space

---
+++ Matt.FountainCodes/InPlaceProblem.cs
@@ -0,0 +1,62 @@
+﻿namespace Matt.FountainCodes
+{
+    using System.IO;
+    using System.IO.MemoryMappedFiles;
+    using Accelerated;
+    using GaussianElimination;
+    using MemoryMappedFiles;
+
+    public class InPlaceProblem : IGaussianProblem
+    {
+        readonly FileStream _coefficientsFile;
+        readonly FileStream _dataFile;
+        readonly int _rowWidth;
+
+        public InPlaceProblem(
+            FileStream coefficientsFile,
+            FileStream dataFile,
+            int numCoefficients,
+            int rowWidth)
+        {
+            NumCoefficients = numCoefficients;
+            _coefficientsFile = coefficientsFile;
+            _dataFile = dataFile;
+            _rowWidth = rowWidth;
+        }
+
+        public bool HasCoefficient(int row, int coefficient)
+        {
+            var address = row * NumCoefficients + coefficient;
+            if (address >= _coefficientsFile.Length)
+                return false;
+            _coefficientsFile.Position = address;
+            return _coefficientsFile.ReadByte() > 0;
+        }
+
+        public void Xor(int from, int to)
+        {
+            // XOR coefficients first
+            using (var fromMemory = MemoryMappedFileHelper.CreateMemoryManager(_coefficientsFile, MemoryMappedFileAccess.Read, out _, from * NumCoefficients, NumCoefficients, true))
+            using (var toMemory = MemoryMappedFileHelper.CreateMemoryManager(_coefficientsFile, MemoryMappedFileAccess.ReadWrite, out _, to * NumCoefficients, NumCoefficients, true))
+            {
+                Bitwise.Xor(
+                    fromMemory.GetSpan(),
+                    toMemory.GetSpan()
+                );
+            }
+
+            // Now XOR the data
+            using (var fromMemory = MemoryMappedFileHelper.CreateMemoryManager(_dataFile, MemoryMappedFileAccess.Read, out _, from * _rowWidth, _rowWidth, true))
+            using (var toMemory = MemoryMappedFileHelper.CreateMemoryManager(_dataFile, MemoryMappedFileAccess.ReadWrite, out _, to * _rowWidth, _rowWidth, true))
+            {
+                Bitwise.Xor(
+                    fromMemory.GetSpan(),
+                    toMemory.GetSpan()
+                );
+            }
+        }
+
+        public int NumCoefficients { get; }
+        public int NumRows => (int) (_dataFile.Length / _rowWidth);
+    }
+}
--- Matt.FountainCodes/Program.cs
+++ Matt.FountainCodes/Program.cs
@@ -2,85 +2,12 @@
 
 namespace Matt.FountainCodes
 {
-    using System.Buffers;
     using System.Collections.Generic;
     using System.IO;
-    using System.IO.MemoryMappedFiles;
     using System.Linq;
-    using System.Reactive.Disposables;
     using System.Text;
     using Matt.Accelerated;
     using Matt.GaussianElimination;
-    using Matt.MemoryMappedFiles;
-
-    class MyGaussianProblem : IGaussianProblem, IDisposable
-    {
-        readonly List<FileStream> _streams = new();
-        readonly int _rowWidth;
-        readonly Action<string> _log;
-        readonly MostUsedCache<int, MemoryManager<byte>> _cache;
-
-        public MyGaussianProblem(int numCoefficients, int rowWidth, Action<string> log)
-        {
-            NumCoefficients = numCoefficients;
-            _rowWidth = rowWidth;
-            _log = log;
-            _cache = new MostUsedCache<int, MemoryManager<byte>>(
-                i =>
-                {
-                    // log($"+++ {i}");
-                    return MemoryMappedFileHelper.CreateMemoryManager(
-                        _streams[i],
-                        MemoryMappedFileAccess.ReadWrite,
-                        out _,
-                        keepOpen: true);
-                },
-                (i, x) =>
-                {
-                    // log($"--- {i}");
-                    ((IDisposable) x).Dispose();
-                },
-                3
-            );
-        }
-
-        public int NumCoefficients { get; }
-
-        public int NumRows => _streams.Count;
-
-        public void Add(FileStream stream)
-        {
-            if (!stream.CanRead)
-                throw new Exception("Can't read from this stream");
-            if (!stream.CanWrite)
-                throw new Exception("Can't write to this stream");
-            if (!stream.CanSeek)
-                throw new Exception("Can't seek within this stream");
-            if (stream.Length != _rowWidth + NumCoefficients)
-                throw new Exception("Incorrect stream length");
-            _streams.Add(stream);
-        }
-
-        public void Dispose() => _cache.Dispose();
-
-        public bool HasCoefficient(int row, int coefficient)
-        {
-            var stream = _streams[row];
-            stream.Position = coefficient;
-            return stream.ReadByte() > 0;
-        }
-
-        public void Xor(int from, int to)
-        {
-            var memoryManagerFrom = _cache.Get(from);
-            var memoryManagerTo = _cache.Get(to);
-            Bitwise.Xor(
-                memoryManagerFrom.GetSpan(),
-                memoryManagerTo.GetSpan()
-            );
-            _log($"XOR {from} -> {to}");
-        }
-    }
 
     class Program
     {
@@ -90,33 +17,26 @@ namespace Matt.FountainCodes
             // Formula for estimating how many XOR operations will be needed:
             // y = 0.4992 * x * x + 3.1447 * x - 31.738
 
-            using var logFile = new FileStream("log.txt", FileMode.Create, FileAccess.Write, FileShare.Read, 4096, FileOptions.SequentialScan);
-            using var logger = new StreamWriter(logFile, new UTF8Encoding(false));
-
             // Set up the Gaussian Elimination problem using files on disk
-            using var disposable = new CompositeDisposable();
             const int numCoefficients = 32;
             const int rowWidth = 1024 * 1024;
             const int numSlices = numCoefficients + 5;
-            using var problem = new MyGaussianProblem(numCoefficients, rowWidth, logger.WriteLine);
 
-            // Generate the message on disk
-            FileStream message;
+            // Generate the message
+            var message = new byte[(long)numCoefficients * rowWidth];
             {
-                message = new FileStream("message.txt", FileMode.Create, FileAccess.ReadWrite, FileShare.Read);
-                disposable.Add(message);
-                const long desiredLength = (long)numCoefficients * rowWidth;
-                var line = $"This message was encoded at {DateTime.Now} on {Environment.MachineName}. This is a test of the emergency broadcast system. Do not be alarmed, this is only a test.";
-                using (var writer = new StreamWriter(message, new UTF8Encoding(false), 4096, true))
+                var line = Encoding.UTF8.GetBytes($"This message was encoded at {DateTime.Now} on {Environment.MachineName}. This is a test of the emergency broadcast system. Do not be alarmed, this is only a test.");
+                for (var i = 0; i < message.Length - line.Length; i += line.Length)
                 {
-                    while (message.Length < desiredLength)
-                    {
-                        writer.WriteLine(line);
-                        writer.Flush();
-                    }
+                    line.CopyTo(message, i);
                 }
-                message.SetLength(desiredLength);
-                message.Flush();
+                Array.Copy(
+                    line,
+                    0,
+                    message,
+                    message.Length / line.Length * line.Length,
+                    message.Length % line.Length
+                );
             }
 
             // Populate the slices with the message
@@ -131,12 +51,7 @@ namespace Matt.FountainCodes
                 .Take(numSlices)
                 .Select((x, i) => (x, i));
             var buffer = new byte[rowWidth];
-            using var source = new SlidingMemoryMappedFile(
-                message,
-                MemoryMappedFileAccess.Read,
-                true,
-                rowWidth * 10
-            );
+            var slices = new List<FileStream>();
             foreach (var (rental, i) in enumerable)
             {
                 using var _ = rental;
@@ -148,7 +63,7 @@ namespace Matt.FountainCodes
                     if (!coefficients[j])
                         continue;
                     Bitwise.Xor(
-                        source.GetMemory(j * rowWidth, rowWidth).Span,
+                        message.AsSpan(j * rowWidth, rowWidth),
                         buffer
                     );
                 }
@@ -169,13 +84,38 @@ namespace Matt.FountainCodes
                 Array.Clear(buffer, 0, buffer.Length);
 
                 // Make the slice part of the problem to solve
-                problem.Add(stream);
-                disposable.Add(stream);
+                slices.Add(stream);
             }
 
-            // Solve the Gaussian Elimination problem
-            if (!GaussianSolver.Solve(problem))
-                throw new Exception("Couldn't solve it");
+            // Combine all the slices into one file
+            using (var coefficientsFile = new FileStream("coefficients.dat", FileMode.Create, FileAccess.ReadWrite, FileShare.Read))
+            using (var solutionFile = new FileStream("solution.dat", FileMode.Create, FileAccess.ReadWrite, FileShare.Read))
+            {
+                var coefficientsBuffer = new byte[numCoefficients];
+                foreach (var sliceFile in slices)
+                {
+                    sliceFile.Position = 0;
+                    if (sliceFile.Read(coefficientsBuffer) != coefficientsBuffer.Length)
+                        throw new Exception("Didn't read enough bytes");
+                    coefficientsFile.Write(coefficientsBuffer);
+                    sliceFile.CopyTo(solutionFile);
+
+                    coefficientsFile.Flush();
+                    solutionFile.Flush();
+                    sliceFile.Dispose();
+                    File.Delete(sliceFile.Name);
+                }
+
+                // Solve the Gaussian Elimination problem
+                var problem = new InPlaceProblem(
+                    coefficientsFile,
+                    solutionFile,
+                    numCoefficients,
+                    rowWidth
+                );
+                if (!GaussianSolver.Solve(problem))
+                    throw new Exception("Couldn't solve it");
+            }
         }
     }
 }
